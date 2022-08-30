b1d2a8eb59a78d52c32b94b47210d8115a44ad83
Yang Zhang <y.zhang@bioc.uzh.ch>
2022-08-30 16:36:09 +0200

    Add functions in draw

--- draw.py
+++ draw.py
@@ -1,6 +1,10 @@
 import rdkit.Chem as Chem
 from IPython.display import Image
 from rdkit.Chem import AllChem, Draw
+import requests
+import json
+import base64
+from PIL import Image as pilimage
 
 def drawGridMols(results, legends=[], mpr=5):
   print(f"Drawing grid molecules {len(results)}")
@@ -13,5 +17,25 @@ def drawGridMols(results, legends=[], mpr=5):
   img.save("/tmp/molgrid.png")
   return Image("/tmp/molgrid.png")
 
-
+def show_png(themol, savefile="/tmp/example_image.png"):
+  json_data = {
+    'structure': themol,
+    'parameters': "png",
+    'filterChain': [
+      {
+        'filter': 'standardizer',
+        'parameters': {
+          'standardizerDefinition': 'aromatize',
+        },
+      },
+    ],
+  }
+  response = requests.post('https://marvinjs-demo.chemaxon.com/rest-v1/util/calculate/molExport', json=json_data)
+  if response.status_code != 200: print(response.status_code, response.reason, response.url, response.text)
+  imgstr = json.loads(response.text)["binaryStructure"];
+  image = base64.b64decode(imgstr);
+  with open(savefile, 'wb') as f:
+    f.write(image)
+  image = pilimage.open(savefile)
+  image.show()
 
